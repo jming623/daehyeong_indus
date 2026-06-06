b3ff5a9e3f29870d500b791c9e7e3f47771cd6df
jming623 <jming623@gmail.com>
2026-06-06 10:31:04 +0900

    fix: commit config.js to repo to prevent intermittent 404 on GitHub Pages

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const EMAILJS_CONFIG = {
+    publicKey: 'vKj_L72njvm4yZJ0L',
+    serviceId: 'service_yx3htjd',
+    templateId: 'template_g5dv0hv'
+};
+
+const COMPANY_CONTACT = {
+    phone: '02-1234-5678',    
+    email: 'info@daehyung.co.kr',
+    address: '서울특별시 강남구 테헤란로 123, 대형빌딩 5층'
+};
